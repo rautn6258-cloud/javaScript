4580c770aeb675a362a6f13e649b0de61ed2690f
rautn6258-cloud <rautn6258@gmail.com>
2025-12-26 16:41:11 +0000

    container confi

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const accountId = 14445
+let accountEmail="rautn6258@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+// accountId=4 //
+accountEmail = "rautn6258@gmail.com"
+accountPassword = "123456"
+accountCity = "japan"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
