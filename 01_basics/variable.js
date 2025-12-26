6790794e510711cef6d19eb2daaa8fd269445404
rautn6258-cloud <rautn6258@gmail.com>
2025-12-26 16:57:26 +0000

    container confi variable

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -2,9 +2,11 @@ const accountId = 14445
 let accountEmail="rautn6258@gmail.com"
 var accountPassword="12345"
 accountCity="jaipur"
+let accountState;
 // accountId=4 //
+
 accountEmail = "rautn6258@gmail.com"
 accountPassword = "123456"
 accountCity = "japan"
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
