89b16689956910185cbfa1bd3d9b7d480ddf6580
rautn6258-cloud <rautn6258@gmail.com>
2026-01-03 17:51:10 +0000

    conversionofdatatypes.js

---
+++ 01_basics/conversionofdatatypes.js
@@ -0,0 +1,17 @@
+let Score = "Neha"
+console.log(typeof Score);//for identifying the datatypes
+console.log(typeof(Score));
+let valueInNumber = Number(Score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let SomeNumber = 33
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+
+
---
+++ 01_basics/variable_dtatypes.js
@@ -0,0 +1,15 @@
+"use strict";//tret all js code as newew version
+//alert(3+3//we are using node js not browser)
+
+//primitiv DataTypes//
+let name = "neha"
+let age = 12
+let isLoggedIn= false //number=>2 to powers3
+//bigint=>use in stock marketting
+//string=<"" use in double qoute for string
+//boolean=>true/false
+//null=>standalone value it is an object its is representstion empty value 
+//undefined=>declaring variable not define value
+//symbol=>uniqenesss find use the symbol
+//object
+console.log(typeof age);
