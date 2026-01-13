c1736947281de73afbbce57b2ad2d6b78c28c392
rautn6258-cloud <rautn6258@gmail.com>
2026-01-13 17:25:20 +0000

    stackand heap.js

---
+++ 01_basics/stackandHeap.js
@@ -0,0 +1,13 @@
+let myYoutube= "ApekshRaut.com"
+let anothername = "myYoutubename"
+anothername = "Chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+let user1 = {
+    email:"rautn6258@gmail.com",
+    upi: "9067849239@ybl"
+}
+let user2 = user1
+user2.email = "rautn6258@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
